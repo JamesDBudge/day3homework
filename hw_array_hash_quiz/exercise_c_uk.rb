182f33c28ea6cb1edce1971604b7915da748880b
James Budge <thebudgie@gmail.com>
2019-04-03 17:24:56 +0100

    completed lab and hw

--- hw_array_hash_quiz/exercise_c_uk.rb
+++ hw_array_hash_quiz/exercise_c_uk.rb
@@ -23,7 +23,7 @@ p united_kingdom[1][:capital]
 
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
 
-united_kingdom[3] = {name: "Northern Ireland", population: 1811000, capital: "Belfast"}
+united_kingdom.push({name: "Northern Ireland", population: 1811000, capital: "Belfast"})
 p united_kingdom[3]
 # 3. Use a loop to print the names of all the countries in the UK.
 
--- optional_lab_array_hash_loop/start_point/my_functions.rb
+++ optional_lab_array_hash_loop/start_point/my_functions.rb
@@ -3,3 +3,26 @@ def add_array_lengths(ar1, ar2)
   length2 = ar2.length()
   return length1 + length2
 end
+
+
+def sum_array(numbers)
+  array_sum = 0
+  for num in numbers
+    array_sum += num
+  end
+  return array_sum
+end
+
+
+def find_item(array, item)
+  for house in array
+    if house == item
+      return true
+    end
+  end
+  return false
+end
+
+def get_first_key(array)
+  return array.keys[0]
+end
